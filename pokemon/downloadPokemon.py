42d85fd967cddc80036a69484efcf77e9229a3b9
nozana <noor.zana@capgemini.com>
2022-10-17 16:23:44 +0100

    New updated downloaded data

---
+++ pokemon/downloadPokemon.py
@@ -0,0 +1,30 @@
+import requests
+import json
+
+
+class FetchData:
+    def fetchdata(self):
+
+        DATA_URL = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151"
+        content = requests.get(DATA_URL)
+        parsedJson = json.loads(content.text)
+
+        dict = {}
+
+
+        pokemons = parsedJson["results"]
+
+        for pokemon in pokemons:
+            pokemonName = pokemon["name"]
+            pokemonURL = pokemon["url"]
+            pokeContent = requests.get(pokemonURL)
+            pokeJson = json.loads(pokeContent.text)
+            attack = pokeJson["stats"][1]["base_stat"]
+            defense = pokeJson["stats"][2]["base_stat"]
+            artwork = pokeJson["sprites"]["front_default"]
+            types = pokeJson["types"]
+            listTypes = []
+            for type in types:
+                listTypes.append(type["type"]["name"])
+            dict[pokemonName] = {"artwork" : artwork, "attack" : attack, "defense" : defense, "types" : listTypes}
+        return dict
