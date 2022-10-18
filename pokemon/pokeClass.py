4a9664e489faad6047ccf73a3ce99e6ed5d414a4
Sayer <becky.sayer@capgemini.com>
2022-10-18 09:23:29 +0100

    created database

---
+++ pokemon/pokeClass.py
@@ -0,0 +1,8 @@
+class Pokemon:
+    def __init__(self, name):
+        self.name = name
+        self.url = ""
+        self.attackValue = 0
+        self.defenseValue = 0
+        self.types = []
+
