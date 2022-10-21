a12d0e89f62a725198716ed7fa63029723c48694
nozana <noor.zana@capgemini.com>
2022-10-21 09:21:59 +0100

    added sound

---
+++ pokemon/pokeMenu.js
@@ -0,0 +1,9 @@
+var audio = new Audio("pokemonSong.mp3");
+function play(){
+   audio.play();
+}
+//for pausing
+//to play from where it left off, use the play method again
+function pause(){
+audio.pause();
+}
